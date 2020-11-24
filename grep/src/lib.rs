8c64f1a09334f222b3c6890c8ff7520c9a854d00
Margarita Krutikova <margaretkru@gmail.com>
2020-11-24 22:49:02 +0100

    Grep: refactor

--- grep/src/lib.rs
+++ grep/src/lib.rs
@@ -1,55 +1,56 @@
 use anyhow::Error;
 use std::fs;
 
-#[derive(Debug, PartialEq)]
-enum Flag {
-    PrintLineNumber,
-    PrintFileNameOnly,
-    CaseInsensitive,
-    InvertResults,
-    MatchEntireLine,
-}
-
 #[derive(Debug)]
 pub struct Flags {
-    list: Vec<Flag>,
+    print_line_number: bool,
+    print_file_name_only: bool,
+    case_insensitive: bool,
+    invert_result: bool,
+    match_entire_line: bool,
 }
 
 impl Flags {
     pub fn new(flags: &[&str]) -> Self {
-        let list = flags
-            .iter()
-            .filter_map(|flag| Flags::parse_flag(flag))
-            .collect();
-        Flags { list }
+        flags.iter().fold(Flags::default_flags(), |mut acc, flag| {
+            match *flag {
+                "-n" => acc.print_line_number = true,
+                "-l" => acc.print_file_name_only = true,
+                "-i" => acc.case_insensitive = true,
+                "-v" => acc.invert_result = true,
+                "-x" => acc.match_entire_line = true,
+                _ => (),
+            }
+            acc
+        })
     }
-    fn parse_flag(flag: &str) -> Option<Flag> {
-        match flag {
-            "-n" => Some(Flag::PrintLineNumber),
-            "-l" => Some(Flag::PrintFileNameOnly),
-            "-i" => Some(Flag::CaseInsensitive),
-            "-v" => Some(Flag::InvertResults),
-            "-x" => Some(Flag::MatchEntireLine),
-            _ => None,
+    fn default_flags() -> Self {
+        Flags {
+            print_file_name_only: false,
+            print_line_number: false,
+            case_insensitive: false,
+            match_entire_line: false,
+            invert_result: false,
         }
     }
 }
 
-fn match_line(pattern: &str, flags: &Vec<Flag>, line: &str) -> bool {
-    match flags.contains(&Flag::MatchEntireLine) {
-        true if line == pattern => true,
-        false if line.contains(pattern) => true,
-        _ => false,
+fn match_text(pattern: &str, flags: &Flags, text: &str) -> bool {
+    if flags.match_entire_line {
+        text == pattern
+    } else {
+        text.contains(pattern)
     }
 }
 
-fn has_match(pattern: &str, flags: &Vec<Flag>, line: &str) -> bool {
-    let matches_pattern = match flags.contains(&Flag::CaseInsensitive) {
-        true => match_line(&pattern.to_lowercase(), flags, &line.to_lowercase()),
-        false => match_line(pattern, flags, line),
+fn has_match(pattern: &str, flags: &Flags, line: &str) -> bool {
+    let matches_pattern = if flags.case_insensitive {
+        match_text(&pattern.to_lowercase(), flags, &line.to_lowercase())
+    } else {
+        match_text(pattern, flags, line)
     };
 
-    match flags.contains(&Flag::InvertResults) {
+    match flags.invert_result {
         false => matches_pattern,
         true if !line.is_empty() => !matches_pattern,
         _ => false,
@@ -57,12 +58,12 @@ fn has_match(pattern: &str, flags: &Vec<Flag>, line: &str) -> bool {
 }
 
 fn format_match(
-    include_line_number: bool,
+    flags: &Flags,
     include_file_name: bool,
     file_name: &str,
     (index, line): (usize, &str),
 ) -> String {
-    let formatted_line = match include_line_number {
+    let formatted_line = match flags.print_line_number {
         true => format!("{}:{}", index + 1, line),
         false => String::from(line),
     };
@@ -74,7 +75,7 @@ fn format_match(
 
 fn match_file_content(
     pattern: &str,
-    flags: &Vec<Flag>,
+    flags: &Flags,
     include_file_name: bool,
     file_name: &str,
     content: &str,
@@ -84,18 +85,14 @@ fn match_file_content(
         .enumerate()
         .filter(|(_, line)| has_match(pattern, flags, line));
 
-    match flags.contains(&Flag::PrintFileNameOnly) {
-        true => match matches.next() {
-            Some(_) => vec![String::from(file_name)],
-            None => vec![],
-        },
-        false => {
-            let include_line_number = flags.contains(&Flag::PrintLineNumber);
-            matches
-                .map(|pair| format_match(include_line_number, include_file_name, file_name, pair))
-                .filter(|result| !result.is_empty())
-                .collect::<Vec<_>>()
-        }
+    match flags.print_file_name_only {
+        true => matches
+            .next()
+            .map_or(vec![], |_| vec![String::from(file_name)]),
+        false => matches
+            .map(|pair| format_match(flags, include_file_name, file_name, pair))
+            .filter(|result| !result.is_empty())
+            .collect::<Vec<_>>(),
     }
 }
 
@@ -105,8 +102,7 @@ pub fn grep(pattern: &str, flags: &Flags, files: &[&str]) -> Result<Vec<String>,
 
     for file_name in files.iter() {
         let content = fs::read_to_string(file_name)?;
-        let matches =
-            match_file_content(pattern, &flags.list, include_file_name, file_name, &content);
+        let matches = match_file_content(pattern, &flags, include_file_name, file_name, &content);
 
         matching_lines.extend(matches);
     }
